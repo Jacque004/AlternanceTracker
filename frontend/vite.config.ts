4ab40b9280f424c74c8e329ccb1f47e96c27ea64
Jacques004 <jacques_p69@icloud.com>
2026-03-27 13:51:31 +0100

    Resolve merge conflict in vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  // Domaine custom GitHub Pages => site servi à la racine.
   base: '/',
   plugins: [react()],
   resolve: {
