7b7dfd350a691440778bfc3ff997315d0008f82a
Jacques004 <jacques_p69@icloud.com>
2026-09-21 10:26:20 +0200

    fix: aligner Node 22 et le lockfile pour débloquer npm ci

---
+++ frontend/eslint.config.js
@@ -0,0 +1,32 @@
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['node_modules/**', 'dist/**', 'build/**'],
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [tseslint.configs.base],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'off',
+      'react-refresh/only-export-components': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
+);
